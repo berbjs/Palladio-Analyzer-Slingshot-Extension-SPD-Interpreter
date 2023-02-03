c1b86425d38e9174edd144b22ad8abb724e447bb
Julijan Katic <julijan@katic.io>
2023-02-03 14:20:03 +0100

    first commit

---
+++ org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/ScalingTriggerInterpreter.java
@@ -0,0 +1,7 @@
+package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter;
+
+import org.palladiosimulator.spd.triggers.util.TriggersSwitch;
+
+public class ScalingTriggerInterpreter extends TriggersSwitch<Object> {
+
+}
---
+++ org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/SpdInterpreter.java
@@ -0,0 +1,7 @@
+package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter;
+
+import org.palladiosimulator.spd.util.SpdSwitch;
+
+public class SpdInterpreter extends SpdSwitch<Object> {
+
+}
