3e9130a96f2e271c4c743929e49c0ff4326ec1a7
Julijan Katic <julijan@katic.io>
2023-08-09 13:07:15 +0200

    Check whether container is in service group

--- org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/utils/TargetGroupUtils.java
+++ org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/utils/TargetGroupUtils.java
@@ -1,5 +1,7 @@
 package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.utils;
 
+import org.palladiosimulator.analyzer.slingshot.core.Slingshot;
+import org.palladiosimulator.pcm.allocation.Allocation;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
 import org.palladiosimulator.spd.targets.ElasticInfrastructure;
 import org.palladiosimulator.spd.targets.ServiceGroup;
@@ -7,11 +9,21 @@ import org.palladiosimulator.spd.targets.TargetGroup;
 
 public class TargetGroupUtils {
 	
+	private static final Allocation allocation = Slingshot.getInstance().getInstance(Allocation.class);
+	
 	public static boolean isContainerInElasticInfrastructure(final ResourceContainer container, final ElasticInfrastructure targetGroup) {
 		return targetGroup.getPCM_ResourceEnvironment()
 						  .getResourceContainer_ResourceEnvironment()
 						  .stream()
 						  .anyMatch(rc -> rc.getId().equals(container.getId()));
+	}
+	
+	public static boolean isContainerInServiceGroup(final ResourceContainer container, final ServiceGroup serviceGroup) {
+		return allocation.getAllocationContexts_Allocation()
+						 .stream()
+						 .filter(ac -> ac.getAssemblyContext_AllocationContext().getId().equals(serviceGroup.getUnitAssembly().getId()))
+						 .map(ac -> ac.getResourceContainer_AllocationContext())
+						 .anyMatch(rc -> rc.getId().equals(container.getId()));
 	}
 
 	public static boolean isContainerInTargetGroup(final ResourceContainer container, 
@@ -19,7 +31,7 @@ public class TargetGroupUtils {
 		if (targetGroup instanceof final ElasticInfrastructure elasticInfrastructure) {
 			return isContainerInElasticInfrastructure(container, elasticInfrastructure);
 		} else if (targetGroup instanceof final ServiceGroup serviceGroup) {
-			return true; // TODO: How do we check this?
+			return isContainerInServiceGroup(container, serviceGroup);
 		}
 		
 		return false;
