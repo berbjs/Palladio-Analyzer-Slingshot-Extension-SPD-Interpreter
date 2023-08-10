4d5399a032ecec56914d0dcc02c28dcf1aa90c8e
Julijan Katic <julijan@katic.io>
2023-08-10 11:51:13 +0200

    Change to naive approach for the median for now

--- org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/aggregator/MedianWindowAggregation.java
+++ org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/aggregator/MedianWindowAggregation.java
@@ -1,16 +1,30 @@
 package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator;
 
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.utils.MedianQueue;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MedianWindowAggregation extends AbstractWindowAggregation {
 
 	public MedianWindowAggregation(final int windowSize) {
-		super(windowSize, new MedianQueue(windowSize));
+		super(windowSize);
 	}
 
 	@Override
 	protected double doAggregation() {
-		return valuesToConsider.peek();
+		if (valuesToConsider.size() == 0) {
+			return 0;
+		}
+		
+		final Double[] values = valuesToConsider.toArray(new Double[0]);
+		Arrays.sort(values);
+		final int i = values.length;
+		
+		if (i % 2 == 0) {
+			return (values[i / 2] + values[i / 2 + 1]) / 2;
+		} else {
+			return (values[(i + 1) / 2]);
+		}
 	}
 
 }
