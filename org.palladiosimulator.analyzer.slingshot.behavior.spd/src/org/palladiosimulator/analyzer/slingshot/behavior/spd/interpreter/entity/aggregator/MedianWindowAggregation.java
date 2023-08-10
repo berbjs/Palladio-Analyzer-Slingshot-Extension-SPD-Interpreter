d331b4356ba0bfd8bb7413152d8c61bcbe798fc7
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-08-10 13:29:43 +0200

    switch +1 to -1 as our indices start with 0.

--- org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/aggregator/MedianWindowAggregation.java
+++ org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/aggregator/MedianWindowAggregation.java
@@ -13,15 +13,15 @@ public class MedianWindowAggregation extends AbstractWindowAggregation {
 		if (valuesToConsider.size() == 0) {
 			return 0;
 		}
-		
+
 		final Double[] values = valuesToConsider.toArray(new Double[0]);
 		Arrays.sort(values);
 		final int i = values.length;
-		
+
 		if (i % 2 == 0) {
-			return (values[i / 2] + values[i / 2 + 1]) / 2;
+			return (values[i / 2] + values[i / 2 - 1]) / 2;
 		} else {
-			return (values[(i + 1) / 2]);
+			return (values[(i - 1) / 2]);
 		}
 	}
 
