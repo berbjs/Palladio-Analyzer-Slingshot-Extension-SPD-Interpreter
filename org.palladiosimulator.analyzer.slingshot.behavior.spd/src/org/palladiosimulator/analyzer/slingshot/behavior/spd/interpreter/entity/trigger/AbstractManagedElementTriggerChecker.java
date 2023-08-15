8d2c007b356a268da05cf22934d9df22f6553a4f
Julijan Katic <julijan@katic.io>
2023-08-15 18:07:21 +0200

    Added some documentation and the missing check

--- org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/trigger/AbstractManagedElementTriggerChecker.java
+++ org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/trigger/AbstractManagedElementTriggerChecker.java
@@ -8,22 +8,31 @@ import javax.measure.quantity.Dimensionless;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entities.FilterObjectWrapper;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entities.FilterResult;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.FixedLengthWindowAggregation;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.utils.TargetGroupUtils;
 import org.palladiosimulator.analyzer.slingshot.common.events.DESEvent;
 import org.palladiosimulator.analyzer.slingshot.monitor.data.entities.SlingshotMeasuringValue;
 import org.palladiosimulator.analyzer.slingshot.monitor.data.events.MeasurementMade;
 import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
 import org.palladiosimulator.metricspec.BaseMetricDescription;
 import org.palladiosimulator.metricspec.MetricSetDescription;
-import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
-import org.palladiosimulator.pcm.resourceenvironment.ProcessingResourceSpecification;
-import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
-import org.palladiosimulator.pcmmeasuringpoint.ActiveResourceMeasuringPoint;
 import org.palladiosimulator.spd.targets.TargetGroup;
 import org.palladiosimulator.spd.triggers.BaseTrigger;
 import org.palladiosimulator.spd.triggers.expectations.ExpectedPrimitive;
 import org.palladiosimulator.spd.triggers.stimuli.ManagedElementsStateStimulus;
 
+/**
+ * This abstract class implements the base functionality for {@link ManagedElementsStateStimulus} elements. Measurements
+ * for this elements need to be aggregated first, which is done by {@link #aggregateMeasurement(MeasurementMade, MeasuringPoint)}.
+ * Afterwards, {@link #getResult(DESEvent)} checks whether enough measurements were made, and whether the calculated value
+ * triggers the policy.
+ * 
+ * Classes that extend this checker need to implement {@link #isMeasuringPointInTargetGroup(MeasuringPoint)}, since each
+ * measuring point has a different way of retrieving the right Resource container.
+ * 
+ * @author Julijan Katic
+ *
+ * @param <T> The concrete element the class is checking for.
+ * @param <MP> The concrete measuring point the checker needs to listen to.
+ */
 public abstract class AbstractManagedElementTriggerChecker<T extends ManagedElementsStateStimulus, MP extends MeasuringPoint> extends TriggerChecker<T> {
 	
 	protected final TargetGroup targetGroup;
@@ -33,6 +42,7 @@ public abstract class AbstractManagedElementTriggerChecker<T extends ManagedElem
 	protected final FixedLengthWindowAggregation aggregator;
 	protected final Class<MP> measuringPointType;
 	
+	@SuppressWarnings("unchecked")
 	public AbstractManagedElementTriggerChecker(final BaseTrigger trigger, 
 												final T stimulus,
 												final Class<MP> measuringPointType,
@@ -81,6 +91,10 @@ public abstract class AbstractManagedElementTriggerChecker<T extends ManagedElem
 	 * disregard.
 	 */
 	protected FilterResult getResult(final DESEvent event) {
+		if (!this.aggregator.isWindowFull()) {
+			return FilterResult.disregard("There are not enough values yet to aggregate.");
+		}
+		
 		final double aggregatedValue = this.aggregator.getCurrentValue();
 		if (this.compareToTrigger(aggregatedValue) == ComparatorResult.IN_ACCORDANCE) {
 			return FilterResult.success(event);
@@ -88,11 +102,9 @@ public abstract class AbstractManagedElementTriggerChecker<T extends ManagedElem
 		return FilterResult.disregard();
 	}
 
-	/*
-	 * Keep history of last n measurements.
-	 */
 	/**
-	 * Helper method to aggregate the measurement in case if the measurement comes from one of the resource containers in the target group.
+	 * Helper method to aggregate the measurement in case if the measurement comes 
+	 * from one of the resource containers in the target group.
 	 */
 	protected void aggregateMeasurement(final MeasurementMade measurementMade, final MP measuringPoint) {
 		if (this.isMeasuringPointInTargetGroup(measuringPoint)
@@ -107,5 +119,15 @@ public abstract class AbstractManagedElementTriggerChecker<T extends ManagedElem
 		return value;
 	}
 	
+	/**
+	 * Checks whether the measuring point is somewhere in the target group and thus, whether
+	 * its measurements are relevant for this trigger. Since each measuring point has a
+	 * different way of retrieving such information, it has to be implemented by the concrete
+	 * trigger.
+	 * 
+	 * @param measuringPoint The measuring point to check.
+	 * @return true if the measuring point lies somewhere in the target group, i.e.
+	 * 		   is relevant for this trigger.
+	 */
 	protected abstract boolean isMeasuringPointInTargetGroup(final MP measuringPoint);
 }
