cf5227812da16411211417b7ee4bb6140abe4310
Floriment Klinaku <klinakuf@gmail.com>
2023-09-30 21:44:32 +0200

    fix for cooldown constraint interpretation

--- org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/constraint/CooldownConstraintFilter.java
+++ org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/constraint/CooldownConstraintFilter.java
@@ -4,6 +4,20 @@ import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entitie
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entities.FilterResult;
 import org.palladiosimulator.spd.constraints.policy.CooldownConstraint;
 
+
+/**
+ * The CooldownConstraint filter evaluates whether the time in which an adjustment 
+ * could potentially occur is exceeding the specified cooldown. 
+ * 
+ * In case the time is within the cooldown then the filter yields success when 
+ * the max number of adjustments within the cooldown has not yet been reached.
+ * 
+ * The state upon which the evaluation occurs is in {@link SPDAdjusterState}. 
+ * The state is altered upon a successful adjustment in the {@link Adjustor} filter. 
+ * 
+ * @author Julijan Katic, Floriment Klinaku
+ *
+ */
 public class CooldownConstraintFilter extends AbstractConstraintFilter<CooldownConstraint> {
 
 	public CooldownConstraintFilter(final CooldownConstraint constraint) {
@@ -12,17 +26,17 @@ public class CooldownConstraintFilter extends AbstractConstraintFilter<CooldownC
 
 	@Override
 	public FilterResult doProcess(final FilterObjectWrapper event) {
-		final int numberScales = event.getState().numberOfScales();
-		final double lastScaling = event.getState().getLatestAdjustmentAtSimulationTime();
+		final int numberScalesInCooldown = event.getState().getNumberOfScalesInCooldown();
+		final double cooldownEnd = event.getState().getCoolDownEnd();
 		
-		if (event.getEventToFilter().time() >= lastScaling + constraint.getCooldownTime()) {
-			if (numberScales < constraint.getMaxScalingOperations()) {
+		if(event.getEventToFilter().time()>=cooldownEnd) {
+			return FilterResult.success(event.getEventToFilter());
+		} else {
+			if (numberScalesInCooldown < constraint.getMaxScalingOperations()) {
 				return FilterResult.success(event.getEventToFilter());
 			} else {
-				return FilterResult.disregard(String.format("Max number scales reached: %d >= %d", numberScales, constraint.getMaxScalingOperations()));
+				return FilterResult.disregard(String.format("Max number scales reached: %d >= %d", numberScalesInCooldown, constraint.getMaxScalingOperations()));
 			}
-		} else {
-			return FilterResult.disregard(String.format("Cooldown not reached yet: %d < %d", event.getEventToFilter().time() - lastScaling, constraint.getCooldownTime()));
 		}
 	}
 
