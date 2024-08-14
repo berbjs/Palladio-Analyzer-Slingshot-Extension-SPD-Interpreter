4248fb37d4879838b05b57a1298c459194e369da
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-08-14 23:51:32 +0200

    Move the model evaluator package + update models for rewards

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/model/LearningBasedModelEvaluator.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/model/LearningBasedModelEvaluator.java
@@ -1,15 +1,17 @@
-package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.models;
+package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model;
 
 import java.util.List;
 
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.ModelAggregatorWrapper;
+import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.reward.RewardEvaluator;
 import org.palladiosimulator.analyzer.slingshot.monitor.data.events.MeasurementMade;
 
 public abstract class LearningBasedModelEvaluator extends ModelEvaluator {
 
     protected final List<ModelAggregatorWrapper<?>> aggregatorList;
 
-    public LearningBasedModelEvaluator(List<ModelAggregatorWrapper<?>> stimuliListeners) {
+    public LearningBasedModelEvaluator(List<ModelAggregatorWrapper<?>> stimuliListeners,
+            RewardEvaluator rewardEvaluator) {
         super();
         this.aggregatorList = stimuliListeners;
     }
