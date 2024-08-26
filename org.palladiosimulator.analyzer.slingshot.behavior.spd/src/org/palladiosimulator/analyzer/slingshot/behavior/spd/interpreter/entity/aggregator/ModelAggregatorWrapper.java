a2af8a80133ba0c1aa6a25ea4e27b3308f4081a4
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-08-26 19:29:09 +0200

    Refactor Aggregation to allow for averaging of non-aggregated stimuli

--- org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/aggregator/ModelAggregatorWrapper.java
+++ org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/aggregator/ModelAggregatorWrapper.java
@@ -3,24 +3,12 @@ package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity
 import org.palladiosimulator.analyzer.slingshot.monitor.data.events.MeasurementMade;
 import org.palladiosimulator.metricspec.BaseMetricDescription;
 import org.palladiosimulator.metricspec.MetricSetDescription;
-import org.palladiosimulator.spd.targets.TargetGroup;
 import org.palladiosimulator.spd.triggers.stimuli.Stimulus;
 
 public abstract class ModelAggregatorWrapper<T extends Stimulus> {
 
-    protected TargetGroup targetGroup;
-    protected T managedElementsStateStimulus;
     protected MetricSetDescription metricSetDescription;
     protected BaseMetricDescription baseMetricDescription;
-
-    public ModelAggregatorWrapper(final T stimulus, final TargetGroup targetGroup,
-            final MetricSetDescription metricSetDescription, final BaseMetricDescription baseMetricDescription) {
-
-        this.targetGroup = targetGroup;
-        this.managedElementsStateStimulus = stimulus;
-        this.metricSetDescription = metricSetDescription;
-        this.baseMetricDescription = baseMetricDescription;
-    }
 
     public abstract double getResult() throws Exception; // TODO IMPORTANT think about whether this
                                                          // exception is necessary, how it should be
