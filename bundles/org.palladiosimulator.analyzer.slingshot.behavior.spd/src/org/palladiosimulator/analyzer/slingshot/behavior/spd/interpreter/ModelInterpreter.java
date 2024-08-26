2880d5b7a1cd3741392dc7b462ae6a88e5eba9b2
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-08-26 19:17:19 +0200

    ModelInterpreter: Handle non-aggregated stimuli
    Currently, the average over the last interval is taken for
    non-aggregated stimuli

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/ModelInterpreter.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/ModelInterpreter.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
+import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.AggregatedStimulusAggregator;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.AnyStimulusAggregator;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.ManagedElementAggregator;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.ModelAggregatorWrapper;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.models.ImprovedQLearningModelEvaluator;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.models.ModelEvaluator;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.models.QThresholdsModelEvaluator;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.models.RandomModelEvaluator;
-import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
-import org.palladiosimulator.spd.adjustments.models.ImprovedQLearningModel;
-import org.palladiosimulator.spd.adjustments.models.QThresholdsModel;
-import org.palladiosimulator.spd.adjustments.models.RandomModel;
-import org.palladiosimulator.spd.adjustments.models.util.ModelsSwitch;
+import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.ImprovedQLearningModelEvaluator;
+import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.ModelEvaluator;
+import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.QThresholdsModelEvaluator;
+import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.RandomModelEvaluator;
+import org.palladiosimulator.spd.models.ImprovedQLearningModel;
+import org.palladiosimulator.spd.models.QThresholdsModel;
+import org.palladiosimulator.spd.models.RandomModel;
+import org.palladiosimulator.spd.models.util.ModelsSwitch;
+import org.palladiosimulator.spd.triggers.AGGREGATIONMETHOD;
 import org.palladiosimulator.spd.triggers.stimuli.AggregatedStimulus;
 import org.palladiosimulator.spd.triggers.stimuli.ManagedElementsStateStimulus;
 import org.palladiosimulator.spd.triggers.stimuli.Stimulus;
@@ -31,27 +32,25 @@ public class ModelInterpreter extends ModelsSwitch<ModelEvaluator> {
     private List<ModelAggregatorWrapper<?>> getStimuliListeners(EList<Stimulus> stimuli) {
         List<ModelAggregatorWrapper<?>> stimuliListenerList = new ArrayList<>();
         for (Stimulus stimulus : stimuli) {
-            stimuliListenerList.add(extracted(stimulus));
+            stimuliListenerList.add(getAggregatorForStimulus(stimulus));
         }
         return stimuliListenerList;
     }
 
     @SuppressWarnings("rawtypes")
-    public ModelAggregatorWrapper extracted(Stimulus stimulus) {
+    public ModelAggregatorWrapper getAggregatorForStimulus(Stimulus stimulus) {
         int windowSize = ((int) ((ModelBasedAdjustment) this.triggerInterpreter.policy.getAdjustmentType())
             .getUsedModel()
             .getInterval());
         if (stimulus instanceof AggregatedStimulus aggregatedStimulus) {
-            return new AnyStimulusAggregator<>(aggregatedStimulus, this.triggerInterpreter.policy.getTargetGroup(),
-                    windowSize, MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE_TUPLE,
-                    MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE);
+            return new AggregatedStimulusAggregator<>(aggregatedStimulus, windowSize);
         } else if (stimulus instanceof ManagedElementsStateStimulus managedElementsStateStimulus) {
-            return new ManagedElementAggregator<>(managedElementsStateStimulus,
-                    this.triggerInterpreter.policy.getTargetGroup(), windowSize,
-                    MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE_TUPLE,
-                    MetricDescriptionConstants.UTILIZATION_OF_ACTIVE_RESOURCE);
+            return new ManagedElementAggregator<>(managedElementsStateStimulus, windowSize);
+        } else {
+            // TODO IMPORTANT currently using average aggregation by default for non-aggregated
+            // stimuli, this might need to be changed
+            return new AnyStimulusAggregator<>(stimulus, windowSize, AGGREGATIONMETHOD.AVERAGE);
         }
-        return null; // TODO IMPORTANT FIXME
     }
 
     @Override
