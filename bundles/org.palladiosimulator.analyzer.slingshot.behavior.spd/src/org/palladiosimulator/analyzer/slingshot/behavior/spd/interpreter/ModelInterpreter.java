963617c0fdceb28653bd97d11d20565876ac531f
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-08-28 18:03:51 +0200

    Implement more useful random model (see SPD metamodel)

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/ModelInterpreter.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/ModelInterpreter.java
@@ -54,7 +54,7 @@ public class ModelInterpreter extends ModelsSwitch<ModelEvaluator> {
 
     @Override
     public ModelEvaluator caseRandomModel(RandomModel model) {
-        return new RandomModelEvaluator();
+        return new RandomModelEvaluator(model);
     }
 
     @Override
