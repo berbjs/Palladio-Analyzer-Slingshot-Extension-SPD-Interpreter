56e7de935d2df16183f7c75abed841cdb84aec8d
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-09-24 00:32:56 +0200

    Cleanup: Revert changes to ModelAdjustmentRequested

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd.data/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/data/ModelAdjustmentRequested.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd.data/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/data/ModelAdjustmentRequested.java
@@ -6,44 +6,21 @@ import org.palladiosimulator.analyzer.slingshot.common.events.AbstractSimulation
 import org.palladiosimulator.spd.ScalingPolicy;
 
 /**
- * Tells that a certain adjustment to the PCM model is requested. An adjustor
- * module can then perform these adjustments.
- * <br>
- * This event holds the enacted scaling policy that was fired as well as
- * the target group.
+ * Tells that a certain adjustment to the PCM model is requested. An adjustor module can then
+ * perform these adjustments. <br>
+ * This event holds the enacted scaling policy that was fired as well as the target group.
  * 
  * @author Julijan Katic
  */
 public final class ModelAdjustmentRequested extends AbstractSimulationEvent implements SpdBasedEvent {
 
-	private final ScalingPolicy scalingPolicy;
-	private final ScalingDirection scalingDirection;
-	private final int scalingMagnitude;
-	
-	public ModelAdjustmentRequested(final ScalingPolicy scalingPolicy, final ScalingDirection scalingDirection, final int scalingMagnitude) {
-		this.scalingPolicy = Objects.requireNonNull(scalingPolicy);
-		this.scalingDirection = scalingDirection;
-		this.scalingMagnitude = scalingMagnitude;
-	}
-	
-	public ModelAdjustmentRequested(final ScalingPolicy scalingPolicy) {
-		this.scalingPolicy = Objects.requireNonNull(scalingPolicy);
-		this.scalingDirection = ScalingDirection.UNDEFINED;
-		this.scalingMagnitude = 0;
-	}
-	
-	public ScalingPolicy getScalingPolicy() {
-		return this.scalingPolicy;
-	}
-	
-	public ScalingDirection getScalingDirection() {
-		return this.scalingDirection;
-	}
+    private final ScalingPolicy scalingPolicy;
 
-	/**
-	 * @return the magnitude of scaling if {@link #getScalingDirection()}  != {@link ScalingDirection#UNDEFINED}, else 0
-	 */
-	public int getScalingMagnitude() {
-		return this.scalingMagnitude;
-	}
+    public ModelAdjustmentRequested(final ScalingPolicy scalingPolicy) {
+        this.scalingPolicy = Objects.requireNonNull(scalingPolicy);
+    }
+
+    public ScalingPolicy getScalingPolicy() {
+        return this.scalingPolicy;
+    }
 }
--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd.data/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/data/ScalingDirection.java
+++
@@ -1,7 +0,0 @@
-package org.palladiosimulator.analyzer.slingshot.behavior.spd.data;
-
-public enum ScalingDirection {
-	UP,
-	DOWN,
-	UNDEFINED;
-}
