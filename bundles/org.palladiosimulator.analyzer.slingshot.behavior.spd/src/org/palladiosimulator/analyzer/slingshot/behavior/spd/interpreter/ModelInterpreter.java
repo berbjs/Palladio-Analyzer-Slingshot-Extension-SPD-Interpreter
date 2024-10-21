bc9a185780703196ae1469e1c1457c68b8f463f4
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-10-21 23:38:53 +0200

    Remove old models

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/ModelInterpreter.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/ModelInterpreter.java
@@ -3,13 +3,9 @@ package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.AnyStimulusAggregator;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.ManagedElementAggregator;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.ModelAggregatorWrapper;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.ImprovedQLearningModelEvaluator;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.ModelEvaluator;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.QThresholdsModelEvaluator;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model.RandomModelEvaluator;
-import org.palladiosimulator.spd.models.ImprovedQLearningModel;
 import org.palladiosimulator.spd.models.LearningBasedModel;
-import org.palladiosimulator.spd.models.QThresholdsModel;
 import org.palladiosimulator.spd.models.RandomModel;
 import org.palladiosimulator.spd.models.util.ModelsSwitch;
 import org.palladiosimulator.spd.triggers.AGGREGATIONMETHOD;
@@ -33,16 +29,5 @@ public class ModelInterpreter extends ModelsSwitch<ModelEvaluator> {
     @Override
     public ModelEvaluator caseRandomModel(RandomModel model) {
         return new RandomModelEvaluator(model);
-    }
-
-    @Override
-    public ModelEvaluator caseQThresholdsModel(QThresholdsModel model) {
-        return new QThresholdsModelEvaluator(model, getAggregatorForStimulus(model.getInput(), model),
-                getAggregatorForStimulus(model.getUtilizationStimulus(), model));
-    }
-
-    @Override
-    public ModelEvaluator caseImprovedQLearningModel(ImprovedQLearningModel model) {
-        return new ImprovedQLearningModelEvaluator(model, getAggregatorForStimulus(model.getInput(), model));
     }
 }
