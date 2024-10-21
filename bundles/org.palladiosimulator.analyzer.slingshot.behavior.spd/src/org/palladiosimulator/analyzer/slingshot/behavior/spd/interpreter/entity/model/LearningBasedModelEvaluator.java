da3d2f59dc3d482284a786b78e829de8af45e9b9
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-10-21 23:36:47 +0200

    Improve LearningBasedModelEvaluator
    - Introduced convenience constructor
    - Better documentation
    - Support printing the trained model

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/model/LearningBasedModelEvaluator.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/model/LearningBasedModelEvaluator.java
@@ -1,5 +1,6 @@
 package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.ModelAggregatorWrapper;
@@ -10,10 +11,28 @@ public abstract class LearningBasedModelEvaluator extends ModelEvaluator {
 
     final List<ModelAggregatorWrapper<?>> aggregatorList;
 
+    /**
+     * @param stimuliListeners
+     *            For all contained ModelAggregaatorWrappers, received measurements will be
+     *            aggregated (if the {@link #recordUsage(MeasurementMade)} method isn't overwritten)
+     */
     LearningBasedModelEvaluator(List<ModelAggregatorWrapper<?>> stimuliListeners, boolean changeOnStimulus,
             boolean changeOnInterval) {
         super();
         this.aggregatorList = stimuliListeners;
+        this.changeOnStimulus = changeOnStimulus;
+        this.changeOnInterval = changeOnInterval;
+    }
+
+    /**
+     * If this method is used, the {@link #recordUsage(MeasurementMade)} method should be
+     * overwritten by a child class, as no Stimuli Listeners are given. If this is not intended (and
+     * some stimulus aggregation needs to happen) use the constructor
+     * {@link #LearningBasedModelEvaluator(List, boolean, boolean)
+     */
+    protected LearningBasedModelEvaluator(boolean changeOnStimulus, boolean changeOnInterval) {
+        super();
+        this.aggregatorList = Collections.emptyList();
         this.changeOnStimulus = changeOnStimulus;
         this.changeOnInterval = changeOnInterval;
     }
@@ -29,4 +48,11 @@ public abstract class LearningBasedModelEvaluator extends ModelEvaluator {
     public abstract void update() throws NotEmittableException;
 
     abstract void recordRewardMeasurement(MeasurementMade measurement);
+
+    /**
+     * Print the values that were obtained during the training phase of the model (so far). Which
+     * values depend on the model, this could i.e. be the Q-Values, some determined thresholds or
+     * similar.
+     */
+    public abstract void printTrainedModel();
 }
