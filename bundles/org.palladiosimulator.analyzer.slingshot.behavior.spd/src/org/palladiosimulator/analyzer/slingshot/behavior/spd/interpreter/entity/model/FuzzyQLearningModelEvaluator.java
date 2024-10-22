405aea5d52526b439e08f032e4aa7a4073608f22
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-10-22 17:41:49 +0200

    Fix + Refactor FuzzyQLearningModelEvaluator
    - Improve readability by introducing new methods
    - Remove unneeded methods
    - Improve comments
    - Improve reward + add fuzzy membership parameters

--- bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/model/FuzzyQLearningModelEvaluator.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.behavior.spd/src/org/palladiosimulator/analyzer/slingshot/behavior/spd/interpreter/entity/model/FuzzyQLearningModelEvaluator.java
@@ -1,15 +1,12 @@
 package org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.model;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.apache.log4j.Logger;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.ModelInterpreter;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.ModelAggregatorWrapper;
 import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.aggregator.NotEmittableException;
-import org.palladiosimulator.analyzer.slingshot.behavior.spd.interpreter.entity.trigger.ModelBasedTriggerChecker;
 import org.palladiosimulator.analyzer.slingshot.monitor.data.events.MeasurementMade;
 import org.palladiosimulator.spd.models.FuzzyQLearningModel;
 import org.palladiosimulator.spd.triggers.stimuli.OperationResponseTime;
@@ -18,13 +15,13 @@ public class FuzzyQLearningModelEvaluator extends LearningBasedModelEvaluator {
 
     private record State(Double utilization, Double responseTime) {
 
-        private static double ALPHA = 0;
-        private static double BETA = 0;
-        private static double GAMMA = 0;
-        private static double DELTA = 0;
-        private static double LAMBDA = 0;
-        private static double MU = 0;
-        private static double NU = 0;
+        private static double ALPHA = 0.2;
+        private static double BETA = 0.4;
+        private static double GAMMA = 0.65;
+        private static double DELTA = 0.9;
+        private static double LAMBDA;
+        private static double MU;
+        private static double NU;
 
         static State createFromModelAggregators(final FuzzyQLearningModelEvaluator fqlmeval)
                 throws NotEmittableException {
@@ -51,12 +48,12 @@ public class FuzzyQLearningModelEvaluator extends LearningBasedModelEvaluator {
 
         double[] getFuzzyResponseTime() {
             final double[] fuzzyValues = { 0.0, 0.0, 0.0 };
-            if (this.utilization < State.LAMBDA) {
+            if (this.responseTime < State.LAMBDA) {
                 fuzzyValues[0] = 1;
-            } else if (this.utilization < State.MU) {
+            } else if (this.responseTime < State.MU) {
                 fuzzyValues[0] = 1 - (this.utilization - State.LAMBDA) / (State.MU - State.LAMBDA);
                 fuzzyValues[1] = 1 - fuzzyValues[0];
-            } else if (this.utilization < NU) {
+            } else if (this.responseTime < NU) {
                 fuzzyValues[1] = 1 - (this.utilization - State.MU) / (State.NU - State.MU);
                 fuzzyValues[2] = 1 - fuzzyValues[1];
             } else {
@@ -68,7 +65,7 @@ public class FuzzyQLearningModelEvaluator extends LearningBasedModelEvaluator {
         /**
          * Returns the firing degree of the state for the given (fuzzy) workload and response time
          * (given as integers here)
-         * 
+         *
          * @param fuzzyWLState
          *            fuzzy workload
          * @param fuzzyRTState
@@ -78,23 +75,34 @@ public class FuzzyQLearningModelEvaluator extends LearningBasedModelEvaluator {
         double getFiringDegree(final int fuzzyWLState, final int fuzzyRTState) {
             return this.getFuzzyResponseTime()[fuzzyRTState] * this.getFuzzyUtil()[fuzzyWLState];
         }
+
+        double[][] getFiringDegrees() {
+            final double[] fuzzyRT = this.getFuzzyResponseTime();
+            final double[] fuzzyWL = this.getFuzzyUtil();
+            final double[][] firingDegrees = new double[3][3];
+            for (int wl = 0; wl < 3; wl++) {
+                for (int rt = 0; rt < 3; rt++) {
+                    firingDegrees[wl][rt] = fuzzyWL[wl] * fuzzyRT[rt];
+                }
+            }
+            return firingDegrees;
+        }
     }
 
-    State old_state = null;
-    Integer old_action = null;
-    State current_state = null;
+    State previousState = null;
+    Integer previousAction = null;
+    State currentState = null;
     private final double discountFactor;
     private final double epsilon;
     private final double learningRate;
     private final double targetResponseTime;
     private final ModelAggregatorWrapper<OperationResponseTime> responseTimeAggregator;
-    private final ModelAggregatorWrapper workloadAggregator;
+    private final ModelAggregatorWrapper<?> workloadAggregator;
     private final double[][][] qValues;
 
-    private final Map<Double, Map<Double, Map<Long, Double>>> Q;
     private int[][] ai;
     private double approximatedQValue;
-    private static final Logger LOGGER = Logger.getLogger(ModelBasedTriggerChecker.class);
+    private static final Logger LOGGER = Logger.getLogger(FuzzyQLearningModelEvaluator.class);
 
     public FuzzyQLearningModelEvaluator(final FuzzyQLearningModel model) {
         super(false, true);
@@ -106,72 +114,80 @@ public class FuzzyQLearningModelEvaluator extends LearningBasedModelEvaluator {
         this.responseTimeAggregator = modelInterpreter.getAggregatorForStimulus(model.getResponseTimeStimulus(), model,
                 model.getResponseTimeAggregationMethod());
         this.workloadAggregator = modelInterpreter.getAggregatorForStimulus(model.getWorkloadStimulus(), model);
+        // Step 1: Initialize Q-Values
         this.qValues = new double[3][3][5];
-        Q = new HashMap<>();
-    }
-
-    private int fuzzy_util(final double util) {
-        return 0;
-    }
-
-    private int fuzzy_response(final double response) {
-        return 0;
-    }
-
-    /**
-     * This function first calculates the firing degree (for the current state in
-     * {@link #current_state} and then multiplies it with the corresponding action and sums these
-     * multiplications up to get something that is called "control action" in the original paper
-     * 
-     * @return
-     */
-    private double[][] calculateFuzzyOutput() {
-        final double[][] alpha = new double[3][3];
-        for (int wl = 0; wl <= 2; wl += 1) {
-            for (int rt = 0; rt <= 2; rt += 1) {
-                alpha[wl][rt] = 1.0;
-            }
-        }
-        // final double consequent = 0; // TODO Where to get the consequent from?
-        for (int wl = 0; wl <= 2; wl += 1) {
-            for (int rt = 0; rt <= 2; rt += 1) {
-                alpha[wl][rt] *= this.current_state.getFiringDegree(wl, rt);
-            }
-        }
-        return alpha;
+        // TODO this should probably be tuned
+        State.LAMBDA = 0.5 * this.targetResponseTime;
+        State.MU = this.targetResponseTime;
+        State.NU = 1.5 * this.targetResponseTime;
     }
 
     @Override
     public void update() throws NotEmittableException {
-        this.current_state = State.createFromModelAggregators(this);
-        LOGGER.info("Utilization: " + this.current_state.utilization);
-        if (this.old_state != null) {
+        this.currentState = State.createFromModelAggregators(this);
+        // TODO the following two lines are there only for debug purposes, remove them
+        State currentState = this.currentState;
+        double[][] firingDegrees = this.currentState.getFiringDegrees();
+        LOGGER.info("Utilization: " + this.currentState.utilization);
+        LOGGER.info("Response time: " + this.currentState.responseTime);
+        if (this.previousState != null) {
             // Step 6: Observe the reinforcement signal r(t + 1) + calculate value for new state
-            // TODO currently using reward from MathLab implementation, not from paper!
             final double reward = this.calculateReward();
             LOGGER.info("Reward (for the last period): " + reward);
-            double value = 0;
-            for (int wl = 0; wl < 3; wl += 1) {
-                for (int rt = 0; rt < 3; rt += 1) {
-                    value += this.current_state.getFiringDegree(wl, rt) * Arrays.stream(this.qValues[wl][rt])
-                        .max()
-                        .getAsDouble();
-                }
-            }
+            double value = calculateValueFunction();
             // Step 7: Calculate the error signal
             double errorSignal = reward + this.discountFactor * value - this.approximatedQValue;
             // Step 8: Update q-Values
             for (int wl = 0; wl < 3; wl += 1) {
                 for (int rt = 0; rt < 3; rt += 1) {
                     this.qValues[wl][rt][ai[wl][rt]] += this.learningRate * errorSignal
-                            * this.old_state.getFiringDegree(wl, rt);
+                            * this.previousState.getFiringDegree(wl, rt);
                 }
             }
         }
-        // this below basically copies the implementation from `fqlearn`
-        this.ai = new int[3][3];
+        // Step 2: Select an action
+        this.ai = choosePartialAction();
+        // Step 3: Calculate control action a
+        double a = calculateControlAction(this.ai);
+        // Step 4: Approximate the Q-function
+        this.approximatedQValue = this.approximateQFunction(this.currentState, ai);
+        // Logging things + setting various things for next iteration
+        LOGGER.info("Current Q-Values: ");
+        for (int wl = 0; wl < 3; wl++) {
+            for (int rt = 0; rt < 3; rt++) {
+                LOGGER.info("q-Values for workload " + wl + " and response time " + rt + ": "
+                        + Arrays.toString(this.qValues[wl][rt]));
+            }
+        }
+        // Step 5: Take action a and let system go to next state (-> in next iteration)
+        this.previousAction = (int) Math.round(a);
+        this.previousState = this.currentState;
+    }
+
+    double calculateValueFunction() {
+        double value = 0;
+        for (int wl = 0; wl < 3; wl += 1) {
+            for (int rt = 0; rt < 3; rt += 1) {
+                value += this.currentState.getFiringDegree(wl, rt) * Arrays.stream(this.qValues[wl][rt])
+                    .max()
+                    .getAsDouble();
+            }
+        }
+        return value;
+    }
+
+    double calculateControlAction(int[][] partialActions) {
         double a = 0;
-        // fuzzy_action_selector
+        for (int wl = 0; wl < 3; wl += 1) {
+            for (int rt = 0; rt < 3; rt += 1) {
+                a += this.currentState.getFiringDegree(wl, rt) * (partialActions[wl][rt] - 2);
+            }
+        }
+        return a;
+    }
+
+    int[][] choosePartialAction() {
+        ai = new int[3][3];
         for (int wl = 0; wl < 3; wl += 1) {
             for (int rt = 0; rt < 3; rt += 1) {
                 if (Math.random() < this.epsilon) {
@@ -191,54 +207,34 @@ public class FuzzyQLearningModelEvaluator extends LearningBasedModelEvaluator {
                 }
             }
         }
-        double qValueChange = 0;
-        for (int wl = 0; wl < 3; wl += 1) {
-            for (int rt = 0; rt < 3; rt += 1) {
-                qValueChange += this.current_state.getFiringDegree(wl, rt) * this.qValues[wl][rt][ai[wl][rt]];
-                a += this.current_state.getFiringDegree(wl, rt) * (ai[wl][rt] - 2);
-            }
-        }
-        LOGGER.info("Current Q-Values: ");
-        for (int wl = 0; wl < 3; wl++) {
-            for (int rt = 0; rt < 3; rt++) {
-                LOGGER.info("q-Values for workload " + wl + " and response time " + rt + ": "
-                        + Arrays.toString(this.qValues[wl][rt]));
-            }
-        }
-        this.approximatedQValue = qValueChange;
-        this.old_action = (int) Math.round(a);
-        this.old_state = this.current_state;
+        return ai;
     }
 
     /**
-     * @deprecated Function for approximating the q function by multiplying alphas with the given
-     *             actions
+     * Function for approximating the q function by multiplying alphas with the given actions
+     * 
      * @param ai
      *            chosen actions for each state, should have size of state space
      * @return
      */
-    private double approximate_q_function(int[][] ai) {
+    double approximateQFunction(State state, int[][] ai) {
         double q = 0;
         for (int i = 0; i <= 3; i++) {
             for (int j = 0; j <= 3; j++) {
-                q = q + this.current_state.getFiringDegree(i, j) * this.qValues[i][j][ai[i][j]];
+                q = q + state.getFiringDegree(i, j) * this.qValues[i][j][ai[i][j]];
             }
         }
         return q;
     }
 
-    private double calculate_value_function(int i, int j) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    private double calculateReward() {
-        if (this.current_state.responseTime < this.targetResponseTime) {
-            return 1;
-        } else if (this.current_state.responseTime < this.old_state.responseTime && this.old_action > 0) {
+    double calculateReward() {
+        if (this.currentState.responseTime < this.targetResponseTime) {
+            return Math.exp(this.currentState.utilization); // Higher utilization should yield
+                                                            // higher rewards after all!
+        } else if (this.currentState.responseTime < this.previousState.responseTime && this.previousAction > 0) {
             return 0;
         } else {
-            return Math.exp((this.targetResponseTime - this.current_state.responseTime) / this.targetResponseTime) - 1;
+            return Math.exp((this.targetResponseTime - this.currentState.responseTime) / this.targetResponseTime) - 1;
         }
     }
 
@@ -261,8 +257,7 @@ public class FuzzyQLearningModelEvaluator extends LearningBasedModelEvaluator {
 
     @Override
     public int getDecision() throws NotEmittableException {
-        // TODO Auto-generated method stub
-        return this.old_action;
+        return this.previousAction;
     }
 
 }
